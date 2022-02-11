d2c6501089eff2497891ffa20b041596a1c955e0
unknown <silvipujadas@gmail.com>
2022-02-11 13:05:51 +0100

    js

---
+++ 04-JavaScript/proves/JS/index.js
@@ -0,0 +1,18 @@
+//Operadors aritmètics
+var a, b, c;
+a=0;
+console.log("Declaració de variables");
+console.log("a = "+a+"");
+console.log("b = "+b+"");
+console.log("c = "+c+"");
+
+a=123;
+c=2;
+var total = c*a;
+console.log(total);
+
+a=12;
+b=5;
+total = c/a;
+
+
