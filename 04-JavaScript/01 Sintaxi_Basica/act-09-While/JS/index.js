8f931a96c9354712abe07e82a706a2fa91078558
spujadas96 <silvipujadas@gmail.com>
2022-02-28 13:12:32 +0100

    functions

--- 04-JavaScript/01 Sintaxi_Basica/act-09-While/JS/index.js
+++ 04-JavaScript/01 Sintaxi_Basica/act-09-While/JS/index.js
@@ -1,12 +1,12 @@
 /* 1. Utilitzant el Bucle while, demana un número per pantalla i calcula el seu factorial. */
 
-/* var numPantalla = window.prompt('Escriu un numero:');
+var numPantalla = window.prompt('Escriu un numero:');
 var factorial = 1;
 
 while (numPantalla > 1){
     if (factorial = factorial*numPantalla);
     numPantalla--;
-} console.log('El numero factorial es:' + factorial); */
+} console.log('El numero factorial es:' + factorial);  
 
 /* 2. A) Utilitza la declaració do while de JavaScript per programar un simple joc d'endevinar un número del 1 al 10. Segueix el següent passos:
 
@@ -15,27 +15,41 @@ a. El teu programa calcularà un número secret (pots fer servir l’objecte Mat
 let numeroAleatori = Math.random();
 let resultatNum = numeroAleatori * 9 + 1;
 let numSecret = Math.ceil(resultatNum);
-console.log(numSecret); 
+console.log(numSecret);  
 
 /* b. Demana el número que has d’endevinar per pantalla entre 1 i 10 (fent servir el prompt). */
 let numEndevinar;
 
-do {
-    numEndevinar = parseInt(window.prompt('Endevina el numero del 1 al 10')); 
-} while (numEndevinar!=numSecret); 
-
 /* c. Repeteix el punt anterior, fins que no donis amb el número secret */
+let contador = 5;
 
+do { 
+    numEndevinar = window.prompt ('Endevina el numero del 1 al 10. Et queden:' + contador + 'intent/s');
+    
+} while (numEndevinar != numSecret && --contador > 0);  */
 
 /* d. Quan el trobis mostra un missatge per pantalla amb el número d’intents que has fet. */
+let contadorFinal = 6 - contador;
 
+if (numEndevinar==numSecret) {
+    window.alert('Enhorabona, el número és el:' + numSecret + 'i has necessitat' + contadorFinal + 'intents per encertar-lo')
+} else {
+    window.alert('Has utilitzat massa intents! El número és el' + numSecret)
+}   
 
-/* b) Modificar el programa. En aquest cas, a més, s’estableix un número màxim de 5 intents. Si l’usuari encerta el número escollit pel programa en aquests 5
-intents, el programa mostra el següent missatge per pantalla: “Enhorabona, el número és X i has necessitat Y intents per encertar-lo”. Si no encerta el número amb els 5 intents, el programa mostra per pentalla: "Has tilitzat massa intents!
+/* e) Modificar el programa. En aquest cas, a més, s’estableix un número màxim de 5 intents. Si l’usuari encerta el número escollit pel programa en aquests 5 intents, el programa mostra el següent missatge per pantalla: “Enhorabona, el número és X i has necessitat Y intents per encertar-lo”. Si no encerta el número amb els 5 intents, el programa mostra per pentalla: "Has utilitzat massa intents!
 
 El número és X " */ 
 
 
 
-
 /* 3. Fes un programa que vagi demanant diferents paraules per pantalla ( fes us del prompt ), quan introdueixis la paraula Fi, deixaràs de demanar més paraules. I mostraràs per pantalla el número de paraules que has introduït. */
+let paraules;
+let paraulaFinal = 'fi';
+let contador = 0;
+
+do {
+    paraules = window.prompt('Escriu la paraula que vulguis');
+    contador ++;
+} while (paraules != paraulaFinal);
+    console.log('has introduit' + contador + 'paraules');
---
+++ 04-JavaScript/01 Sintaxi_Basica/act-10-Function/JS/index.js
@@ -0,0 +1,92 @@
+/* 1. Crea una funció que es digui Saludar, que tingui tres argument: nom, cognom, sexe. Crida a aquesta funció amb diferents escenaris, per tal d’escriure per pantalla:
+Benvinguda [nom] [Cognoms] si és dona.
+Benvingut [nom] [Cognoms] si és home.
+
+També, tingues en compte un escenari, per quan no passis a la funció el paràmetre sexe. */
+function saludar (nom, cognom, sexe){
+    if (sexe == 'dona') {
+        window.alert('Benvinguda' + nom + cognom)
+    }else if (sexe =='home') {
+        window.alert('Benvingut' + nom + cognom) 
+    }else {
+        window.alert('Hola')
+    }
+}
+
+saludar('Sonia','Lopez','dona');
+saludar('Juan','Lopez','home');
+saludar('','','');
+
+/* 2. Crea una funció que es calculi l’edat d’un gos. Quin parametre podries fer servir?*/
+function edatGos(data) {
+    var avui = 2022;
+    var edat = avui - data;
+    
+
+console.log('El gos té' + edat + 'anys')
+
+if (edat < 5) {
+        return true;
+    } else {
+        return false;
+    }
+}
+var esJove = false;                                   
+
+esJove = edatGos(1990);
+console.log(esJove)
+esJove = edatGos(2020);
+console.log(esJove)
+
+
+// 3. Crea una funció on se li pasa 2 números. I que retorni el menor.
+
+
+/* 4. Programa en JavaScript una funció on se li passi dos arguments: string1 -frase- i string2-frase-, i retorna quants caràcters hi ha en total = string1 + string2. */
+
+
+/* 5. Analitza e investiga les diferencies entre les següents funcions. Aquesta és una pregunta d’entrevista com a JS Developer. (Hoisting)
+sumar();
+function sumar() {
+console.log(2 + 2);
+}
+sumar2();
+const sumar2 = function() {
+console.log(3 + 3);
+} */
+
+/* 6. Donada una adreça de correu, implementa una funció que verifiqui si el email té carregat el caràcter @. */
+
+
+// 7. Defineix una frase e implementar les funcions següents:
+// a. Funció per Imprimir la primera meitat dels caràcters de la cadena.
+// b. Funció per Imprimir el darrer caràcter.
+// c. Funció per Imprimir-ho en forma inversa.
+// d. Funció per Imprimir cada caràcter del String separat amb un guió.
+/* e. Funció per Imprimir la quantitat de vocals sense accents emmagatzemades. */
+
+
+/* 8. Fes una funció que donat un número de DNI, calculi si la lletra és correcta. Tingues present que la lletra del DNI es calcula en dos passos: */
+
+//a. Calcular el mòdul del número del DNI respecte 23
+/* b. El resultat de l’operació anterior ha de correspondre a la posició del següent string:
+
+var lletres =”TRWAGMYFPDXBNJZSQVHLCKE”; */
+
+
+
+/* 9. Fes un programa amb les seves funcions que determini si un password és segur. Per exemple, un password segur ha de complir les següents condicions:
+● Ha de contenir almenys 8 números.
+● Ha de contenir almenys una majúscula.
+● Ha de contenir almenys una minúscula.
+● Ha de contenir un número.
+● Almenys ha de contenir un caràcter especial.
+Pots fer servir una funció per cada comprovació. */
+
+
+
+/* 10. Simula un Captcha senzill de 5 caràcters ( amb lletres minúscules i números ) amb JAVASCRIPT. El captcha el pots generar fent ús random i de la següent variable:
+
+var captchaChars = "1234567890abcdefghijklmnopqrst";
+// Random captchaChars.length
+Pots implementar una funció que generi el captcha i un altre que el validi. */
